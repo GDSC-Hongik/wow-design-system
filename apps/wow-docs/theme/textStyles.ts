c7f4b0c8f06f90386f98a94d334c350c3440aab8
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-04 17:04:00 +0900

    chore: wow-docs textStyle 객체 구조 변경

--- apps/wow-docs/theme/textStyles.ts
+++ apps/wow-docs/theme/textStyles.ts
@@ -3,13 +3,9 @@ import { typography } from "wow-tokens";
 
 export const textStyles = defineTextStyles({
   h1: {
-    value: {
-      fontSize: typography.h1,
-    },
+    value: typography.h1,
   },
   h2: {
-    value: {
-      fontSize: typography.h2,
-    },
+    value: typography.h2,
   },
 });
