677383113f4c182d79c0be3baa885d30e70b7af1
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-06 01:24:59 +0900

    chore: wow-ui panda config 추가

--- packages/wow-ui/panda.config.ts
+++ packages/wow-ui/panda.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "@pandacss/dev";
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
+  minify: true,
+  watch: true,
+  outExtension: "js",
+  polyfill: true,
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
   exclude: [],
   outdir: "styled-system",
