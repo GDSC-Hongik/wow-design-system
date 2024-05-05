da731d9245e9b8a4d442044cf896d55d2c0756a2
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-06 01:24:49 +0900

    chore: wow-docs panda config 추가

--- apps/wow-docs/panda.config.ts
+++ apps/wow-docs/panda.config.ts
@@ -3,8 +3,11 @@ import { textStyles } from "./theme/textStyles";
 import { tokens } from "./theme/tokens";
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
+  minify: true,
+  watch: true,
+  outExtension: "js",
+  polyfill: true,
   include: ["./app/**/*.{ts,tsx,js,jsx}"],
   exclude: [],
   outdir: "styled-system",
