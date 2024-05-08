96529dd0932afccd1506262c4e7da2a0da0e02c6
Eugene Kim <eugene02876@gmail.com>
2024-05-09 02:03:24 +0900

    fix: eslint 루트에서 실행되도록 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,39 @@
 // This configuration only applies to the package manager root.
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
+  plugins: ["only-warn"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+    "styled-system/",
+    "panda.config.ts",
+  ],
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
+
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
