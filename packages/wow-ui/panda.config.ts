e3870a318f18f0453d1da518b86759bf0858c79a
hamo-o <yheel369@gmail.com>
2024-05-09 19:06:07 +0900

    feat: panda config에 불필요한 css를 제거하는 hook 추가

--- packages/wow-ui/panda.config.ts
+++ packages/wow-ui/panda.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "@pandacss/dev";
+import { removeUnusedCssVars } from "./remove-unused-css-vars";
+import { removeUnusedKeyframes } from "./remove-unused-keyframes";
+
 export default defineConfig({
   preflight: true,
   minify: true,
@@ -7,5 +10,12 @@ export default defineConfig({
   polyfill: true,
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
   exclude: [],
+  hooks: {
+    "cssgen:done": ({ artifact, content }) => {
+      if (artifact === "styles.css") {
+        return removeUnusedCssVars(removeUnusedKeyframes(content));
+      }
+    },
+  },
   outdir: "styled-system",
 });
