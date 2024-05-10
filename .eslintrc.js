dbaf9680fb00e11ca4e0a5e7df620fdc23d80fa4
Eugene Kim <eugene02876@gmail.com>
2024-05-11 01:51:19 +0900

    chore: prettier 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,17 @@ const project = resolve(process.cwd(), "tsconfig.json");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "eslint-config-turbo",
+  ],
   plugins: ["only-warn"],
   globals: {
     React: true,
