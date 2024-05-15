23ef33abb855a156ea84ec287eab5ca97afc2ca6
SeieunYoo <isho48239@gmail.com>
2024-05-15 12:14:01 +0900

    refactor: rem단위로 변환

--- packages/wow-tokens/src/typography.ts
+++ packages/wow-tokens/src/typography.ts
@@ -1,63 +1,74 @@
 export const display1 = {
-  fontSize: "40px",
+  fontSize: "2.5rem",
   lineHeight: "130%",
   fontWeight: 700,
+  letterSpacing: "-1%",
 };
 export const display2 = {
-  fontSize: "32px",
+  fontSize: "2rem",
   lineHeight: "130%",
   fontWeight: 700,
+  letterSpacing: "-1%",
 };
 export const h1 = {
-  fontSize: "24px",
+  fontSize: "1.5rem",
   lineHeight: "130%",
   fontWeight: 600,
+  letterSpacing: "-1%",
 };
 
 export const h2 = {
-  fontSize: "18px",
+  fontSize: "1.125rem",
   lineHeight: "130%",
   fontWeight: 600,
+  letterSpacing: "-1%",
 };
 
 export const h3 = {
-  fontSize: "16px",
+  fontSize: "1rem",
   lineHeight: "130%",
   fontWeight: 600,
+  letterSpacing: "-1%",
 };
 
 export const body1 = {
-  fontSize: "16px",
+  fontSize: "1rem",
   lineHeight: "160%",
   fontWeight: 500,
+  letterSpacing: "-1%",
 };
 
 export const body2 = {
-  fontSize: "14px",
+  fontSize: "0.875rem",
   lineHeight: "160%",
   fontWeight: 500,
+  letterSpacing: "-1%",
 };
 
 export const body3 = {
-  fontSize: "12px",
+  fontSize: "0.75rem",
   lineHeight: "140%",
   fontWeight: 500,
+  letterSpacing: "0%",
 };
 
 export const label1 = {
-  fontSize: "16px",
+  fontSize: "1rem",
   lineHeight: "100%",
   fontWeight: 600,
+  letterSpacing: "-1%",
 };
 
 export const label2 = {
-  fontSize: "14px",
+  fontSize: "0.875rem",
   lineHeight: "100%",
   fontWeight: 600,
+  letterSpacing: "0%",
 };
 
 export const label3 = {
-  fontSize: "12px",
+  fontSize: "0.75rem",
   lineHeight: "100%",
   fontWeight: 600,
+  letterSpacing: "0%",
 };
