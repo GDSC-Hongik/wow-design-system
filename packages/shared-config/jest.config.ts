70809c1676b0aaefc3585d2ae747e20968e776ee
hamo-o <yheel369@gmail.com>
2024-05-17 01:46:20 +0900

    feat: 공통 jest config package

--- packages/shared-config/jest.config.ts
+++ packages/shared-config/jest.config.ts
@@ -1,4 +1,4 @@
-import { Config } from "jest";
+import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
@@ -12,10 +12,14 @@ const config: Config = {
     ],
   },
 
+  setupFilesAfterEnv: ["./jest.config.ts"],
   verbose: true,
   collectCoverage: true,
   restoreMocks: true,
-  testMatch: ["<rootDir>/src/**/*.test.(js|jsx|ts|tsx)"],
+  testMatch: [
+    "<rootDir>/src/**/*.test.(js|jsx|ts|tsx)",
+    "<rootDir>/app/**/*.test.(js|jsx|ts|tsx)",
+  ],
   testPathIgnorePatterns: [
     "<rootDir>/styled-system/",
     "<rootDir>/node-modules/",
