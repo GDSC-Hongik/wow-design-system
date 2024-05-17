07075587d883abeae98626b921f558abc067d608
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-17 17:30:07 +0900

    fix: 웹 접근성 테스트 자동화 실패 관련 문제 해결

--- packages/wow-ui/.storybook/test-runner.ts
+++ packages/wow-ui/.storybook/test-runner.ts
@@ -6,6 +6,29 @@ import { injectAxe, checkA11y } from "axe-playwright";
  * to learn more about the test-runner hooks API.
  */
 const config: TestRunnerConfig = {
+  async prepare({ page, browserContext, testRunnerConfig }) {
+    page.setDefaultTimeout(120 * 1000);
+
+    const targetURL = process.env.TARGET_URL;
+    const iframeURL = new URL("iframe.html", targetURL).toString();
+
+    if (testRunnerConfig.getHttpHeaders) {
+      const headers = await testRunnerConfig.getHttpHeaders(iframeURL);
+      await browserContext.setExtraHTTPHeaders(headers);
+    }
+
+    await page.goto(iframeURL, { waitUntil: "load" }).catch((err) => {
+      if (
+        err instanceof Error &&
+        err.message.includes("ERR_CONNECTION_REFUSED")
+      ) {
+        const errorMessage = `Could not access the Storybook instance at ${targetURL}. Are you sure it's running?\n\n${err.message}`;
+        throw new Error(errorMessage);
+      }
+
+      throw err;
+    });
+  },
   async preVisit(page) {
     await injectAxe(page);
   },
