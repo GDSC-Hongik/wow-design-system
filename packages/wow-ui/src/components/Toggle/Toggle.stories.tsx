41af488fb7947cc7b334cfaa9305869d12f626ff
ghdtjgus76 <ghdtjgus76@naver.com>
2024-05-21 21:34:03 +0900

    docs: 토글 컴포넌트 스토리 코드 문서화 추가

--- packages/wow-ui/src/components/Toggle/Toggle.stories.tsx
+++ packages/wow-ui/src/components/Toggle/Toggle.stories.tsx
@@ -6,6 +6,38 @@ const meta = {
   title: "UI/Toggle",
   component: Toggle,
   tags: ["autodocs"],
+  parameters: {
+    componentSubtitle: "토글 컴포넌트",
+  },
+  argTypes: {
+    as: {
+      description:
+        "as는 렌더링할 요소 또는 컴포넌트를 나타냅니다. 기본값은 button입니다.",
+      table: {
+        defaultValue: { summary: "button" },
+      },
+    },
+    initialIsActive: {
+      description:
+        "initialIsActive는 토글 버튼이 처음에 눌려 있는지 여부를 나타냅니다.",
+      table: {
+        defaultValue: { summary: "false" },
+      },
+      control: {
+        type: "boolean",
+      },
+    },
+    isDisabled: {
+      description:
+        "isDisabled는 토글 버튼이 비활성화되어 있는지 여부를 나타냅니다.",
+      table: {
+        defaultValue: { summary: "false" },
+      },
+      control: {
+        type: "boolean",
+      },
+    },
+  },
 } satisfies Meta<typeof Toggle>;
 
 export default meta;
--- packages/wow-ui/src/components/Toggle/index.tsx
+++ packages/wow-ui/src/components/Toggle/index.tsx
@@ -10,7 +10,7 @@ import { forwardRef, useState } from "react";
  * @template T 렌더링할 요소 또는 컴포넌트 타입
  *
  * @param {T} [as] 렌더링할 요소 또는 컴포넌트. 기본값은 button.
- * @param {boolean} [initialIsActive=false] 토글 버튼이 처음에 눌려져 있는지 여부.
+ * @param {boolean} [initialIsActive=false] 토글 버튼이 처음에 눌려 있는지 여부.
  * @param {boolean} [isDisabled=false] 토글 버튼이 비활성화되어 있는지 여부.
  * @param {ComponentPropsWithoutRef<T>} rest 렌더링된 요소 또는 컴포넌트에 전달할 추가 props.
  * @param {ComponentPropsWithRef<T>["ref"]} ref 렌더링된 요소 또는 컴포넌트에 연결할 ref.
