0cac35a9c8f68cf6ff3444ab8b7538c1c7b6e20f
Eugene Kim <eugene02876@gmail.com>
2024-05-22 01:47:28 +0900

    feat: token에 radii 추가

--- packages/theme/src/radius.ts
+++ packages/theme/src/radius.ts
@@ -1,1 +1,15 @@
-import { defineSemanticTokens, defineTokens } from "@pandacss/dev";
+import { defineTokens } from "@pandacss/dev";
+import { radius as wowRadius } from "wowds-tokens";
+
+const radii = defineTokens.radii({
+  borderRadius: {
+    4: { value: wowRadius.radius4 },
+    8: { value: wowRadius.radius8 },
+    12: { value: wowRadius.radius12 },
+    full: { value: wowRadius.radiusFull },
+  },
+});
+
+export const tokens = defineTokens({
+  radii,
+});
