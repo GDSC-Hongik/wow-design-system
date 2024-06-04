0b336ad4726f646939bfcee05784468ee27d5d6a
hamo-o <yheel369@gmail.com>
2024-06-04 10:06:22 +0900

    fix: docs 예시 삭제

--- apps/wow-docs/app/page.tsx
+++ apps/wow-docs/app/page.tsx
@@ -1,7 +1,6 @@
 import { css } from "@styled-system/css/css";
 import Checkbox from "wowds-ui/Checkbox";
 import Chip from "wowds-ui/Chip";
-import { RadioButton, RadioGroup } from "wowds-ui/RadioGroup";
 import Switch from "wowds-ui/Switch";
 
 const Home = () => {
@@ -19,13 +18,6 @@ const Home = () => {
       >
         <p>docs</p>
       </main>
-      <Checkbox />
-      <RadioGroup defaultValue="1학년" name="학년">
-        <RadioButton label="1학년" />
-        <RadioButton label="2학년" />
-        <RadioButton label="3학년" />
-        <RadioButton disabled label="4학년" />
-      </RadioGroup>
     </>
   );
 };
