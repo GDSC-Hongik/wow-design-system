7b70ad2b90c499aad672f2cc57a0578a2d612c65
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-07 12:09:12 +0900

    chore: breakpoint 네이밍 변경

--- packages/theme/src/tokens/breakpoint.ts
+++ packages/theme/src/tokens/breakpoint.ts
@@ -2,7 +2,7 @@ import { breakpoint } from "wowds-tokens";
 
 export const breakpoints = {
   xs: breakpoint.xs,
-  s: breakpoint.s,
-  m: breakpoint.m,
-  l: breakpoint.l,
+  sm: breakpoint.sm,
+  md: breakpoint.md,
+  lg: breakpoint.lg,
 };
