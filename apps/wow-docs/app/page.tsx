e0a106c079f5cbb0c080705e811748de7bc4145e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-08 13:47:14 +0900

    chore: Checkbox 상대 경로 절대 경로로 변경

--- apps/wow-docs/app/page.tsx
+++ apps/wow-docs/app/page.tsx
@@ -1,20 +1,9 @@
-import { css } from "@styled-system/css/css";
+import Checkbox from "wowds-ui/Checkbox";
 import Switch from "wowds-ui/Switch";
-
-import Checkbox from "../../../packages/wow-ui/src/components/Checkbox";
 
 const Home = () => {
   return (
     <>
-      <main
-        className={css({
-          color: "textBlack",
-          textStyle: "h1",
-          bg: "backgroundAlternative",
-        })}
-      >
-        <p>docs</p>
-      </main>
       <Switch />
       <Checkbox />
     </>
