5eda5ace51a2f5cd3e1f197371d620ef48ec3e85
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-09 18:23:25 +0900

    chore: docs에 컴포넌트 추가

--- apps/wow-docs/app/page.tsx
+++ apps/wow-docs/app/page.tsx
@@ -1,5 +1,15 @@
+import Checkbox from "wowds-ui/Checkbox";
+import Chip from "wowds-ui/Chip";
+import Switch from "wowds-ui/Switch";
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Checkbox />
+      <Chip label="Chip" />
+      <Switch />
+    </>
+  );
 };
 
 export default Home;
