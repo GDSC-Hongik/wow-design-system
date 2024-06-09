d7c12741e9609e5f9bf55bf7e6ed375e6c79189e
홍서현 <ghdtjgus76@naver.com>
2024-06-09 22:14:06 +0900

    [Refactor] styled-system 변경 사항 안 잡히도록 설정 수정 (#38)
    * chore: root에 panda codegen, formatting 관련 스크립트 추가
    
    * chore:formatting 관련 스크립트 수정
    
    * chore: styled-system 폴더 포맷팅
    
    * chore: build 스크립트 순서 수정
    
    * chore: husky pre-push hook 추가
    
    * chore: styled-system format 스크립트 styled-system만 적용되도록 수정
    
    * fix : wow-docs 에서 codegen 하는 로직 삭제
    
    ---------
    
    Co-authored-by: SeieunYoo <isho48239@gmail.com>

--- packages/wow-ui/styled-system/patterns/visually-hidden.js
+++ packages/wow-ui/styled-system/patterns/visually-hidden.js
@@ -1,18 +1,19 @@
-import { getPatternStyles, patternFns } from '../helpers.js';
-import { css } from '../css/index.js';
+import { getPatternStyles, patternFns } from "../helpers.js";
+import { css } from "../css/index.js";
 
 const visuallyHiddenConfig = {
-transform(props) {
-  return {
-    srOnly: true,
-    ...props
-  };
-}}
+  transform(props) {
+    return {
+      srOnly: true,
+      ...props,
+    };
+  },
+};
 
 export const getVisuallyHiddenStyle = (styles = {}) => {
-  const _styles = getPatternStyles(visuallyHiddenConfig, styles)
-  return visuallyHiddenConfig.transform(_styles, patternFns)
-}
+  const _styles = getPatternStyles(visuallyHiddenConfig, styles);
+  return visuallyHiddenConfig.transform(_styles, patternFns);
+};
 
-export const visuallyHidden = (styles) => css(getVisuallyHiddenStyle(styles))
-visuallyHidden.raw = getVisuallyHiddenStyle
+export const visuallyHidden = (styles) => css(getVisuallyHiddenStyle(styles));
+visuallyHidden.raw = getVisuallyHiddenStyle;
