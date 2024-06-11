ce7c789f032023928687b1f4115972109e79ab18
hamo-o <yheel369@gmail.com>
2024-06-11 09:54:22 +0900

    chore: docs에서 기능 확인

--- apps/wow-docs/app/page.tsx
+++ apps/wow-docs/app/page.tsx
@@ -1,6 +1,7 @@
 import { css } from "@styled-system/css/css";
 import Checkbox from "wowds-ui/Checkbox";
 import Chip from "wowds-ui/Chip";
+import { RadioButton, RadioGroup } from "wowds-ui/RadioGroup";
 import Switch from "wowds-ui/Switch";
 
 const Home = () => {
@@ -9,6 +10,10 @@ const Home = () => {
       <Checkbox />
       <Chip label="Chip" />
       <Switch />
+      <RadioGroup defaultValue="1학년" name="학년">
+        <RadioButton label="1학년" value="1학년" />
+        <RadioButton label="2학년" value="2학년" />
+      </RadioGroup>
       <main
         className={css({
           color: "textBlack",
