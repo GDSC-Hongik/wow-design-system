660d3960947d10e50f737467aeedd015d5158cd8
SeieunYoo <isho48239@gmail.com>
2024-06-25 01:50:41 +0900

    fix : ReactNode 타입 import 하는 것으로 수정

--- packages/wow-ui/src/hooks/useFlattenChildren.ts
+++ packages/wow-ui/src/hooks/useFlattenChildren.ts
@@ -1,20 +1,18 @@
+import type { ReactNode } from "react";
 import { Children, isValidElement, useCallback, useMemo } from "react";
 
-const useFlattenChildren = (children: React.ReactNode): React.ReactNode[] => {
-  const flattenChildren = useCallback(
-    (children: React.ReactNode): React.ReactNode[] => {
-      const result: React.ReactNode[] = [];
-      Children.forEach(children, (child) => {
-        if (isValidElement(child) && child.props.children) {
-          result.push(...flattenChildren(child.props.children));
-        } else {
-          result.push(child);
-        }
-      });
-      return result;
-    },
-    []
-  );
+const useFlattenChildren = (children: ReactNode): ReactNode[] => {
+  const flattenChildren = useCallback((children: ReactNode): ReactNode[] => {
+    const result: ReactNode[] = [];
+    Children.forEach(children, (child) => {
+      if (isValidElement(child) && child.props.children) {
+        result.push(...flattenChildren(child.props.children));
+      } else {
+        result.push(child);
+      }
+    });
+    return result;
+  }, []);
 
   return useMemo(() => flattenChildren(children), [children, flattenChildren]);
 };
