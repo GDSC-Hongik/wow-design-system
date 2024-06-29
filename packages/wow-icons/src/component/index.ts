ef42050e05ff34035278f56c1b24a6582a6966bd
Eugene Kim <eugene02876@gmail.com>
2024-06-30 01:58:27 +0900

    feature: Help 아이콘 생성

--- packages/wow-icons/src/component/index.ts
+++ packages/wow-icons/src/component/index.ts
@@ -1,3 +1,4 @@
 export { default as Check } from "./Check.tsx";
 export { default as DownArrow } from "./DownArrow.tsx";
+export { default as Help } from "./Help.tsx";
 export { default as RightArrow } from "./RightArrow.tsx";
