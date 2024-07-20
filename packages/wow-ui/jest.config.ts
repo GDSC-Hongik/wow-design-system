5b35c1372e8e02fedd9cc06932e38b36448f2d26
Eugene Kim <eugene02876@gmail.com>
2024-07-20 14:56:56 +0900

    fix: 타입 단언

--- packages/wow-ui/jest.config.ts
+++ packages/wow-ui/jest.config.ts
@@ -2,38 +2,7 @@ import { Config } from "jest";
 import jestConfig from "shared-config/jest.config";
 
 const config: Config = {
-  // ...jestConfig,
-  preset: "ts-jest",
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        tsconfig: "<rootDir>/tsconfig.json",
-      },
-    ],
-  },
-
-  setupFilesAfterEnv: ["../shared-config/jest.setup.ts"],
-  verbose: true,
-  collectCoverage: true,
-  restoreMocks: true,
-  testMatch: [
-    "<rootDir>/src/**/*.test.(js|jsx|ts|tsx)",
-    "<rootDir>/app/**/*.test.(js|jsx|ts|tsx)",
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/styled-system/",
-    "<rootDir>/node-modules/",
-  ],
-  coveragePathIgnorePatterns: [
-    "<rootDir>/styled-system/",
-    "<rootDir>/node-modules/",
-  ],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "^@styled-system(.*)$": "<rootDir>/styled-system/$1",
-  },
+  ...jestConfig,
   testEnvironment: "jsdom",
 };
 
