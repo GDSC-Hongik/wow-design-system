cfa5dc8c8fa381b68d9adb07103b78a461639eb0
Eugene Kim <eugene02876@gmail.com>
2024-07-20 15:46:51 +0900

    chore: jest 설정 변경

--- packages/shared-config/jest.config.ts
+++ packages/shared-config/jest.config.ts
@@ -2,36 +2,10 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        tsconfig: "<rootDir>/tsconfig.json",
-      },
-    ],
-  },
-
   setupFilesAfterEnv: ["../shared-config/jest.setup.ts"],
   verbose: true,
   collectCoverage: true,
   restoreMocks: true,
-  testMatch: [
-    "<rootDir>/src/**/*.test.(js|jsx|ts|tsx)",
-    "<rootDir>/app/**/*.test.(js|jsx|ts|tsx)",
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/styled-system/",
-    "<rootDir>/node-modules/",
-  ],
-  coveragePathIgnorePatterns: [
-    "<rootDir>/styled-system/",
-    "<rootDir>/node-modules/",
-  ],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "^@styled-system(.*)$": "<rootDir>/styled-system/$1",
-  },
 };
 
 export default config;
