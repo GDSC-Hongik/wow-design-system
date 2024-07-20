fc75fdc77cd199f6490992255f9a756ad76c7cf7
Eugene Kim <eugene02876@gmail.com>
2024-07-20 15:28:40 +0900

    fix: 설정 오버로딩

--- packages/wow-ui/jest.config.ts
+++ packages/wow-ui/jest.config.ts
@@ -4,6 +4,15 @@ import jestConfig from "shared-config/jest.config";
 const config: Config = {
   ...jestConfig,
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.json", // packages/wow-ui/tsconfig.json을 가리키게 함
+      },
+    ],
+  },
 };
 
 export default config;
