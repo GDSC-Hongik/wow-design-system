8d78f905f859d9426cb1ac160d9fdcb7fda4c2ae
Eugene Kim <eugene02876@gmail.com>
2024-07-24 14:20:21 +0900

    fix: Jest RootDir 변경

--- packages/shared-config/jest.config.ts
+++ packages/shared-config/jest.config.ts
@@ -2,7 +2,31 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.json",
+      },
+    ],
+  },
+  testMatch: [
+    "<rootDir>/src/**/*.test.(js|jsx|ts|tsx)",
+    "<rootDir>/app/**/*.test.(js|jsx|ts|tsx)",
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/styled-system/",
+    "<rootDir>/node-modules/",
+  ],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/styled-system/",
+    "<rootDir>/node-modules/",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@styled-system(.*)$": "<rootDir>/styled-system/$1",
+  },
   setupFilesAfterEnv: ["../shared-config/jest.setup.ts"],
   verbose: true,
   collectCoverage: true,
--- packages/wow-ui/jest.config.ts
+++ packages/wow-ui/jest.config.ts
@@ -4,31 +4,31 @@ import jestConfig from "shared-config/jest.config";
 const config: Config = {
   ...jestConfig,
   testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        tsconfig: "<rootDir>/tsconfig.json",
-      },
-    ],
-  },
-  testMatch: [
-    "<rootDir>/src/**/*.test.(js|jsx|ts|tsx)",
-    "<rootDir>/app/**/*.test.(js|jsx|ts|tsx)",
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/styled-system/",
-    "<rootDir>/node-modules/",
-  ],
-  coveragePathIgnorePatterns: [
-    "<rootDir>/styled-system/",
-    "<rootDir>/node-modules/",
-  ],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "^@styled-system(.*)$": "<rootDir>/styled-system/$1",
-  },
+  // transform: {
+  //   "^.+\\.jsx?$": "babel-jest",
+  //   "^.+\\.tsx?$": [
+  //     "ts-jest",
+  //     {
+  //       tsconfig: "<rootDir>/tsconfig.json",
+  //     },
+  //   ],
+  // },
+  // testMatch: [
+  //   "<rootDir>/src/**/*.test.(js|jsx|ts|tsx)",
+  //   "<rootDir>/app/**/*.test.(js|jsx|ts|tsx)",
+  // ],
+  // testPathIgnorePatterns: [
+  //   "<rootDir>/styled-system/",
+  //   "<rootDir>/node-modules/",
+  // ],
+  // coveragePathIgnorePatterns: [
+  //   "<rootDir>/styled-system/",
+  //   "<rootDir>/node-modules/",
+  // ],
+  // moduleNameMapper: {
+  //   "^@/(.*)$": "<rootDir>/src/$1",
+  //   "^@styled-system(.*)$": "<rootDir>/styled-system/$1",
+  // },
 };
 
 export default config;
