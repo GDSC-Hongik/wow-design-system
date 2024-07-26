87dca04d40a21d37ad7e2bce1375fece84d35e51
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-26 16:25:16 +0900

    comment: RainbowSpinner 컴포넌트 jsdoc 추가

--- packages/wow-ui/src/components/Spinner/RainbowSpinner.tsx
+++ packages/wow-ui/src/components/Spinner/RainbowSpinner.tsx
@@ -3,6 +3,12 @@ import type { CSSProperties } from "react";
 
 import rainbowSpinner from "@/assets/lottie/rainbowSpinner.json";
 
+/**
+ * @description 레인보우 스피너 컴포넌트입니다.
+ *
+ * @param {CSSProperties["width"]} [width] - 스피너의 너비.
+ * @param {CSSProperties["height"]} [height] - 스피너의 높이.
+ */
 export interface RainbowSpinnerProps {
   width?: CSSProperties["width"];
   height?: CSSProperties["height"];
