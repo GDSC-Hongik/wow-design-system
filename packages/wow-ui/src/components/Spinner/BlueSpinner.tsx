8cd24a7be0e794c81593dc434ee85b59820fd5be
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-28 01:21:01 +0900

    chore: 스피너 컴포넌트 props에 autoPlay, loop 추가

--- packages/wow-ui/src/components/Spinner/BlueSpinner.tsx
+++ packages/wow-ui/src/components/Spinner/BlueSpinner.tsx
@@ -8,16 +8,27 @@ import blueSpinner from "@/assets/lottie/blueSpinner.json";
  *
  * @param {CSSProperties["width"]} [width] - 스피너의 너비.
  * @param {CSSProperties["height"]} [height] - 스피너의 높이.
+ * @param {boolean} [loop] - 애니메이션의 반복 재생 여부. 기본값은 `true`.
+ * @param {boolean} [autoPlay] - 컴포넌트가 마운트될 때 자동으로 애니메이션이 재생될지 여부. 기본값은 `true`.
  */
 export interface BlueSpinnerProps {
   width?: CSSProperties["width"];
   height?: CSSProperties["height"];
+  autoPlay?: boolean;
+  loop?: boolean;
 }
 
-const BlueSpinner = ({ width = 223, height = 200 }: BlueSpinnerProps) => {
+const BlueSpinner = ({
+  width = 223,
+  height = 200,
+  autoPlay = true,
+  loop = true,
+}: BlueSpinnerProps) => {
   return (
     <Lottie
       animationData={blueSpinner}
+      autoPlay={autoPlay}
+      loop={loop}
       style={{
         width,
         height,
--- packages/wow-ui/src/components/Spinner/RainbowSpinner.tsx
+++ packages/wow-ui/src/components/Spinner/RainbowSpinner.tsx
@@ -4,20 +4,31 @@ import type { CSSProperties } from "react";
 import rainbowSpinner from "@/assets/lottie/rainbowSpinner.json";
 
 /**
- * @description 레인보우 스피너 컴포넌트입니다.
+ * @description 레인보우 스피너 컴포넌트입니다. Lottie 애니메이션을 사용하여 스피너를 표시합니다.
  *
  * @param {CSSProperties["width"]} [width] - 스피너의 너비.
  * @param {CSSProperties["height"]} [height] - 스피너의 높이.
+ * @param {boolean} [loop] - 애니메이션의 반복 재생 여부. 기본값은 `true`.
+ * @param {boolean} [autoPlay] - 컴포넌트가 마운트될 때 자동으로 애니메이션이 재생될지 여부. 기본값은 `true`.
  */
 export interface RainbowSpinnerProps {
   width?: CSSProperties["width"];
   height?: CSSProperties["height"];
+  autoPlay?: boolean;
+  loop?: boolean;
 }
 
-const RainbowSpinner = ({ width = 223, height = 200 }: RainbowSpinnerProps) => {
+const RainbowSpinner = ({
+  width = 223,
+  height = 200,
+  autoPlay = true,
+  loop = true,
+}: RainbowSpinnerProps) => {
   return (
     <Lottie
       animationData={rainbowSpinner}
+      autoPlay={autoPlay}
+      loop={loop}
       style={{
         width,
         height,
