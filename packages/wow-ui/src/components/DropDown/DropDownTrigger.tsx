48d2e28af6158dc959da87a2f5bc0ebc2f128edb
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2024-08-09 19:17:28 +0900

    [Refactor]: DropdownTrigger의 타입을 button으로 지정해요. (#129)
    * refac: DropdownTrigger에 button type 추가
    
    * chore: chagneset파일 작성
    
    * chore: pnpm install
    
    * fix: 혹시몰라 workflow 변경해보기

--- packages/wow-ui/src/components/DropDown/DropDownTrigger.tsx
+++ packages/wow-ui/src/components/DropDown/DropDownTrigger.tsx
@@ -74,6 +74,7 @@ const DropDownTrigger = ({
         id={`${dropdownId}-trigger`}
         justifyContent="space-between"
         outline="none"
+        type="button"
         className={dropdownStyle({
           type: open ? "focused" : selectedValue ? "selected" : "default",
         })}
