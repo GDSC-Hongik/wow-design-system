cba6af52b8faf491599cebedad7d1c698c003c10
Eugene Kim <eugene02876@gmail.com>
2024-08-10 22:28:02 +0900

    feat: TableContainer 생성

---
+++ packages/wow-ui/src/components/Table/TableContainer.tsx
@@ -0,0 +1,26 @@
+import { styled } from "@styled-system/jsx";
+import type { PropsWithChildren } from "react";
+import { forwardRef } from "react";
+
+interface TableContainerProps extends PropsWithChildren {}
+
+const TableContainer = forwardRef<HTMLDivElement, TableContainerProps>(
+  (props, ref) => {
+    const { children } = props;
+    return (
+      <styled.div
+        display="block"
+        maxWidth="100%"
+        overflowX="auto"
+        overflowY="hidden"
+        ref={ref}
+        whiteSpace="nowrap"
+        {...props}
+      >
+        {children}
+      </styled.div>
+    );
+  }
+);
+
+export default TableContainer;
