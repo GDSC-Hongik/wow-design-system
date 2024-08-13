ed554f9dc74647c476cb6935d834c0d3d8532f5a
SeieunYoo <isho48239@gmail.com>
2024-08-13 19:03:32 +0900

    feat: 필요한 아이콘 추가

--- packages/wow-icons/src/component/index.ts
+++ packages/wow-icons/src/component/index.ts
@@ -1,7 +1,10 @@
 export { default as Check } from "./Check.tsx";
 export { default as Close } from "./Close.tsx";
 export { default as DownArrow } from "./DownArrow.tsx";
+export { default as Edit } from "./Edit.tsx";
 export { default as Help } from "./Help.tsx";
+export { default as Reload } from "./Reload.tsx";
 export { default as RightArrow } from "./RightArrow.tsx";
 export { default as Search } from "./Search.tsx";
+export { default as Trash } from "./Trash.tsx";
 export { default as Warn } from "./Warn.tsx";
