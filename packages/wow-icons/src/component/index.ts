b6925f71ab4558a9f85a11f4e7f57357893a0948
Eugene Kim <eugene02876@gmail.com>
2024-08-13 15:01:07 +0900

    feat: plus, link 아이콘 추가

--- packages/wow-icons/src/component/index.ts
+++ packages/wow-icons/src/component/index.ts
@@ -2,6 +2,8 @@ export { default as Check } from "./Check.tsx";
 export { default as Close } from "./Close.tsx";
 export { default as DownArrow } from "./DownArrow.tsx";
 export { default as Help } from "./Help.tsx";
+export { default as Link } from "./Link.tsx";
+export { default as Plus } from "./Plus.tsx";
 export { default as RightArrow } from "./RightArrow.tsx";
 export { default as Search } from "./Search.tsx";
 export { default as Warn } from "./Warn.tsx";
