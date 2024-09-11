ec4ab336d13c5db323fe0e32f44ac5c69af6363b
SeieunYoo <isho48239@gmail.com>
2024-09-11 23:38:42 +0900

    feat: TabContent, TabTrigger 에 ref 전달

--- packages/wow-ui/src/components/Tabs/TabsContent.tsx
+++ packages/wow-ui/src/components/Tabs/TabsContent.tsx
@@ -1,28 +1,31 @@
 "use client";
 
-import type { PropsWithChildren } from "react";
+import { forwardRef, type PropsWithChildren } from "react";
 
 import { useTabContext } from "./contexts/TabContext";
 
 interface TabsContentProps extends PropsWithChildren {
   value: string;
 }
-export const TabsContent = ({
-  value: tabValue,
-  children,
-}: TabsContentProps) => {
-  const { value, label } = useTabContext();
-  const selected = tabValue === value;
-  if (!selected) return;
 
-  return (
-    <div
-      aria-labelledby={`${label}-tab-trigger-${value}`}
-      id={`${label}-tab-content-${value}`}
-      role="tabpanel"
-      tabIndex={0}
-    >
-      {children}
-    </div>
-  );
-};
+export const TabsContent = forwardRef<HTMLDivElement, TabsContentProps>(
+  ({ value: tabValue, children }: TabsContentProps, ref) => {
+    const { value, label } = useTabContext();
+    const selected = tabValue === value;
+    if (!selected) return null;
+
+    return (
+      <div
+        aria-labelledby={`${label}-tab-trigger-${value}`}
+        id={`${label}-tab-content-${value}`}
+        ref={ref}
+        role="tabpanel"
+        tabIndex={0}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+TabsContent.displayName = "TabsContent";
--- packages/wow-ui/src/components/Tabs/TabsTrigger.tsx
+++ packages/wow-ui/src/components/Tabs/TabsTrigger.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { cva } from "@styled-system/css";
-import { type PropsWithChildren, useEffect, useRef } from "react";
+import { forwardRef, type PropsWithChildren, useEffect, useRef } from "react";
+
+import { useMergeRefs } from "@/hooks/useMergeRefs";
 
 import { useCollectionContext } from "./contexts/CollectionContext";
 import { useTabContext } from "./contexts/TabContext";
@@ -10,42 +12,47 @@ interface TapTriggerProps extends PropsWithChildren {
   value: string;
 }
 
-export const TabsTrigger = ({ value, children }: TapTriggerProps) => {
-  const { value: selectedValue, setSelectedValue, label } = useTabContext();
-  const selected = selectedValue === value;
+export const TabsTrigger = forwardRef<HTMLButtonElement, TapTriggerProps>(
+  ({ value, children }: TapTriggerProps, ref) => {
+    const { value: selectedValue, setSelectedValue, label } = useTabContext();
+    const selected = selectedValue === value;
 
-  const handleClickTabTrigger = () => {
-    setSelectedValue(value);
-  };
+    const handleClickTabTrigger = () => {
+      setSelectedValue(value);
+    };
 
-  const { values } = useCollectionContext();
-  const buttonRef = useRef<HTMLButtonElement>(null);
+    const { values } = useCollectionContext();
+    const internalButtonRef = useRef<HTMLButtonElement>(null);
+    const buttonRef = useMergeRefs(ref, internalButtonRef);
 
-  useEffect(() => {
-    const currentItem = values?.find((item) => item === value);
-    if (!currentItem) {
-      values?.push(value);
-    }
-    if (selected && buttonRef.current) {
-      buttonRef.current.focus();
-    }
-  }, [values, selected, value]);
+    useEffect(() => {
+      const currentItem = values?.find((item) => item === value);
+      if (!currentItem) {
+        values?.push(value);
+      }
+      if (selected && internalButtonRef.current) {
+        internalButtonRef.current.focus();
+      }
+    }, [values, selected, value]);
 
-  return (
-    <button
-      aria-controls={`${label}-tab-content-${value}`}
-      aria-selected={selected}
-      className={tabTriggerStyle({ type: selected ? "selected" : "default" })}
-      id={`${label}-tab-trigger-${value}`}
-      ref={buttonRef}
-      role="tab"
-      tabIndex={selected ? 0 : -1}
-      onClick={handleClickTabTrigger}
-    >
-      {children}
-    </button>
-  );
-};
+    return (
+      <button
+        aria-controls={`${label}-tab-content-${value}`}
+        aria-selected={selected}
+        className={tabTriggerStyle({ type: selected ? "selected" : "default" })}
+        id={`${label}-tab-trigger-${value}`}
+        ref={buttonRef}
+        role="tab"
+        tabIndex={selected ? 0 : -1}
+        onClick={handleClickTabTrigger}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+TabsTrigger.displayName = "TabsTrigger";
 
 const tabTriggerStyle = cva({
   base: {
