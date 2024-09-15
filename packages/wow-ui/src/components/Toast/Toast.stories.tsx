6a25417a89c414ea1ea29b7ec3c8397eebd9e3e8
hamo-o <yheel369@gmail.com>
2024-09-15 17:45:36 +0900

    feat: 아이콘 클릭 함수 스토리 추가, state 활용 추가

--- packages/wow-ui/src/components/Toast/Toast.stories.tsx
+++ packages/wow-ui/src/components/Toast/Toast.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
 import { Warn } from "wowds-icons";
 
 import Toast from ".";
@@ -31,6 +32,11 @@ const meta: Meta<typeof Toast> = {
       description: "Toast 컴포넌트가 사라지도록 하는 트리거 함수를 나타냅니다.",
       control: false,
     },
+    onClickArrowIcon: {
+      description:
+        "Toast 컴포넌트의 화살표 아이콘을 클릭했을 때 호출되는 함수를 나타냅니다.",
+      control: false,
+    },
     icon: {
       description: "Toast 좌측에 들어갈 아이콘을 나타냅니다.",
       control: false,
@@ -50,48 +56,77 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    text: "Text",
-    subText: "subtext",
-  },
+export const Default = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button style={{ cursor: "pointer" }} onClick={() => setOpen(true)}>
+        토스트 표시
+      </button>
+      {open && (
+        <Toast
+          id="toast1"
+          subText="subtext"
+          text="Text"
+          onRemove={() => setOpen(false)}
+        />
+      )}
+    </>
+  );
 };
 
-export const Close: Story = {
-  args: {
-    text: "Text",
-    subText: "subtext",
-    type: "close",
-  },
+export const Close = () => {
+  const [open, setOpen] = useState(true);
+
+  return (
+    open && (
+      <Toast
+        id="toast2"
+        subText="subtext"
+        text="Text"
+        type="close"
+        onRemove={() => setOpen(false)}
+      />
+    )
+  );
 };
 
 export const Arrow: Story = {
   args: {
+    id: "toast3",
     text: "Text",
     subText: "subtext",
     type: "arrow",
+    onRemove: () => {},
   },
 };
 
 export const Icon: Story = {
   args: {
+    id: "toast4",
     text: "Text",
     subText: "subtext",
     icon: <Warn />,
+    onRemove: () => {},
   },
 };
 
 export const IconArrow: Story = {
   args: {
+    id: "toast5",
     text: "Text",
     subText: "subtext",
     type: "arrow",
     icon: <Warn />,
+    onRemove: () => {},
   },
 };
 
 export const TwoLines: Story = {
   args: {
+    id: "toast6",
     text: "TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText",
+    onRemove: () => {},
   },
 };
