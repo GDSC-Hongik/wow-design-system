244e1a03fe7dab5bcf2adc3d59ed33cbf6b09980
SeieunYoo <isho48239@gmail.com>
2024-11-13 04:24:52 +0900

    feat: 필요없는 파일 삭제, panda preset 에 textStyle 추가

--- apps/wow-docs/app/page.tsx
+++ apps/wow-docs/app/page.tsx
@@ -1,16 +1,4 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import Button from "wowds-ui/Button";
-import Checkbox from "wowds-ui/Checkbox";
-import Chip from "wowds-ui/Chip";
-import Divider from "wowds-ui/Divider";
-import DropDown from "wowds-ui/DropDown";
-import DropDownOption from "wowds-ui/DropDownOption";
-import MultiGroup from "wowds-ui/MultiGroup";
-import RadioButton from "wowds-ui/RadioButton";
-import RadioGroup from "wowds-ui/RadioGroup";
-import SearchBar from "wowds-ui/SearchBar";
-import Switch from "wowds-ui/Switch";
 
 import { routePath } from "@/constants/routePath";
 
