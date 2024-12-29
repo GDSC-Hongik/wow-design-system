97aeb58e49fcdacb3aaacf4d3e001051bafe445f
hamo-o <yheel369@gmail.com>
2024-12-30 01:59:31 +0900

    feat: Card 컴포넌트 컨텐츠 추가할 수 있도록

--- apps/wow-docs/components/Card.tsx
+++ apps/wow-docs/components/Card.tsx
@@ -1,9 +1,17 @@
 import { css } from "@styled-system/css";
-import type { PropsWithChildren } from "react";
+import type { CSSProperties, PropsWithChildren } from "react";
 
-interface CardProps extends PropsWithChildren {}
-const Card = ({ children }: CardProps) => {
-  return <div className={containerStyle}>{children}</div>;
+interface CardProps extends PropsWithChildren {
+  isBackground?: boolean;
+  style?: CSSProperties;
+}
+
+const Card = ({ children, isBackground = false, style }: CardProps) => {
+  return (
+    <div className={containerStyle} {...(style && { style })}>
+      {isBackground ? <div className={contentStyle}>{children}</div> : children}
+    </div>
+  );
 };
 
 export default Card;
@@ -14,5 +22,15 @@ const containerStyle = css({
   padding: "32px 40px",
 
   display: "flex",
+  justifyContent: "center",
+});
+
+const contentStyle = css({
+  width: "100%",
+  height: "100%",
+  background: "white",
+
+  display: "flex",
+  alignItems: "center",
   justifyContent: "center",
 });
