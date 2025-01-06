8cf9e81297f9ee5449073ed7e048ee8c486dd4f9
soulchicken <soulfever01@naver.com>
2025-01-06 21:40:58 +0900

    feat: color 페이지 마크업

---
+++ apps/wow-docs/app/foundation/color/page.tsx
@@ -0,0 +1,71 @@
+import Card from "@components/Card";
+import ImageCards from "@components/ImageCards";
+import Space from "@components/Space";
+import Text from "@components/Text";
+import Title from "@components/Text/Title";
+import {
+  componentDescriptions,
+  globalDescription,
+  semanticDescriptions,
+} from "@constants/document/foundation/color";
+import Image from "next/image";
+import type { Metadata } from "next/types";
+import Divider from "wowds-ui/Divider";
+
+export const metadata: Metadata = {
+  title: "Color",
+  description: "와우 디자인 시스템의 Color 입니다.",
+};
+
+const ColorPage = () => {
+  return (
+    <>
+      <Title
+        main="Color"
+        sub="컬러는 사용자가 브랜드 아이덴티티와 기능을 인지하는 것을 돕는 시각적 요소입니다."
+        variant="header"
+      />
+      <Space height={68} />
+      <Text as="h2" typo="display2WebPage">
+        Semantic
+      </Text>
+      <Text color="sub" typo="body1">
+        전체 컬러 팔레트 중 서비스에 사용되는 컬러를 용도별로 지정해서
+        시스템화해요.
+      </Text>
+      <Space height={40} />
+      <ImageCards items={semanticDescriptions} />
+      <Text as="h3" typo="headingWebPage">
+        Component
+      </Text>
+      <Text color="sub" typo="body1">
+        UI 내 요소들에 사용되는 컬러로, State별로 나누어서 정리했어요.
+        <br />
+        정리된 토큰 외 컬러 사용시 변형 및 확장이 가능해요.
+      </Text>
+      <Space height={20} />
+      <ImageCards items={componentDescriptions} />
+      <Divider />
+      <Space height={40} />
+      <Text as="h2" typo="display2WebPage">
+        Global
+      </Text>
+      <Text color="sub" typo="body1">
+        모든 컬러의 셰이드를 지정했어요. 그래픽 및 UI 요소를 확장할 시 이 안에서
+        참조하여 사용해요.
+      </Text>
+      <Space height={40} />
+      <Card>
+        <Image
+          alt={globalDescription.imageAlt}
+          height={globalDescription.imageHeight}
+          src={globalDescription.imageSrc}
+          width={globalDescription.imageWidth}
+        />
+      </Card>
+      <Space height={40} />
+    </>
+  );
+};
+
+export default ColorPage;
