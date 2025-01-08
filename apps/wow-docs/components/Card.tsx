98db6014600ae9a8ed78c32f7c6304ab2dcbc49a
SeieunYoo <isho48239@gmail.com>
2025-01-08 15:26:51 +0900

    feat: 드롭다운 페이지 마크업

--- apps/wow-docs/components/Card.tsx
+++ apps/wow-docs/components/Card.tsx
@@ -1,9 +1,12 @@
 import { css } from "@styled-system/css";
+import { clsx } from "clsx";
 import type { PropsWithChildren } from "react";
 
-interface CardProps extends PropsWithChildren {}
-const Card = ({ children }: CardProps) => {
-  return <div className={containerStyle}>{children}</div>;
+interface CardProps extends PropsWithChildren {
+  className?: string;
+}
+const Card = ({ children, className }: CardProps) => {
+  return <div className={clsx(containerStyle, className)}>{children}</div>;
 };
 
 export default Card;
@@ -12,4 +15,6 @@ const containerStyle = css({
   backgroundColor: "backgroundAlternative",
   borderRadius: "sm",
   padding: "32px 40px",
+  display: "flex",
+  justifyContent: "center",
 });
