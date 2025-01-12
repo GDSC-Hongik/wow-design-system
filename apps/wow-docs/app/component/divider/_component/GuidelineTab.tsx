d876071c6f891ca361c6c5a59b5f5908821cfbb6
hamo-o <yheel369@gmail.com>
2025-01-12 15:43:12 +0900

    feat: Divider 컴포넌트 탭 마크업

---
+++ apps/wow-docs/app/component/divider/_component/GuidelineTab.tsx
@@ -0,0 +1,18 @@
+import ImageCards from "@components/ImageCards";
+import Space from "@components/Space";
+import Text from "@components/Text";
+
+const GuidelineTab = () => {
+  return (
+    <>
+      <Space height={48} />
+      <Text as="h2" typo="display2WebPage">
+        Guideline
+      </Text>
+      <Space height={40} />
+      <Space height={40} />
+    </>
+  );
+};
+
+export default GuidelineTab;
