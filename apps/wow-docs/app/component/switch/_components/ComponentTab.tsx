581135e124c7a339dc134c8e44714a8918e8a73a
soulchicken <soulfever01@naver.com>
2025-01-24 22:36:17 +0900

    feat: switch ComponentTab 컴포넌트

--- apps/wow-docs/app/component/switch/_components/ComponentTab.tsx
+++ apps/wow-docs/app/component/switch/_components/ComponentTab.tsx
@@ -1,5 +1,37 @@
+import Card from "@components/Card";
+import Space from "@components/Space";
+import Text from "@components/Text";
+import Image from "next/image";
+import Switch from "wowds-ui/Switch";
+
 const ComponentTab = () => {
-  return <div>ComponentTab</div>;
+  return (
+    <>
+      <Space height={48} />
+      <Text typo="display2WebPage">Component</Text>
+      <Space height={40} />
+      <Text typo="headingWebPage">Btn</Text>
+      <Space height={20} />
+      <Card style={{ padding: "32px auto" }}>
+        <Image
+          alt="Switch 버튼의 checked, unchecked 상태를 dafault, hover, pressed, disabled로 분류해서 보여주는 이미지"
+          height={188}
+          src="/component/switch/switch-component-btn.svg"
+          width={166}
+        />
+      </Card>
+      <Space height={40} />
+      <Text typo="headingWebPage">Btn + Text</Text>
+      <Space height={20} />
+      <Card
+        style={{
+          padding: "52px auto",
+        }}
+      >
+        <Switch defaultChecked label="Text" />
+      </Card>
+    </>
+  );
 };
 
 export default ComponentTab;
