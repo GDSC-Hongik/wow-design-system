776e54d0210d8260e06b2656aa0c36645ba1e6c7
soulchicken <soulfever01@naver.com>
2025-01-27 00:07:08 +0900

    refactor: checkbox componentTab 상수화, map 순회

--- apps/wow-docs/app/component/checkbox/_components/ComponentTab.tsx
+++ apps/wow-docs/app/component/checkbox/_components/ComponentTab.tsx
@@ -1,36 +1,8 @@
-import Card from "@components/Card";
 import Space from "@components/Space";
 import Text from "@components/Text";
-import { Flex } from "@styled-system/jsx";
-import type { CheckboxProps } from "wowds-ui/Checkbox";
-import Checkbox from "wowds-ui/Checkbox";
 
-import Checkboxes from "@/component/checkbox/_components/Checkboxes";
-
-type CheckboxItem = CheckboxProps & {
-  key: number;
-  pressed?: boolean;
-};
-
-const checkboxAndTexts: CheckboxItem[] = [
-  {
-    key: 1,
-    position: "vertical",
-    label: "Text",
-    defaultChecked: true,
-  },
-  {
-    key: 2,
-    position: "vertical",
-    label: "Text",
-  },
-  {
-    key: 3,
-    label: "Text",
-    defaultChecked: true,
-  },
-  { key: 4, label: "Text" },
-];
+import CheckboxAndTextCard from "@/component/checkbox/_components/CheckboxAndTextCard";
+import CheckboxCard from "@/component/checkbox/_components/CheckboxCard";
 
 const ComponentTab = () => {
   return (
@@ -40,21 +12,11 @@ const ComponentTab = () => {
       <Space height={40} />
       <Text typo="headingWebPage">Box</Text>
       <Space height={20} />
-      <Card>
-        <Flex gap="12px">
-          <Checkboxes />
-        </Flex>
-      </Card>
+      <CheckboxCard />
       <Space height={40} />
       <Text typo="headingWebPage">Box + Text</Text>
       <Space height={20} />
-      <Card>
-        <Flex gap="2rem">
-          {checkboxAndTexts.map((props) => (
-            <Checkbox {...props} />
-          ))}
-        </Flex>
-      </Card>
+      <CheckboxAndTextCard />
     </>
   );
 };
--- apps/wow-docs/constants/store/index.ts
+++ apps/wow-docs/constants/store/index.ts
@@ -1,3 +1,4 @@
 export * from "./buttonData";
+export * from "./checkboxData";
 export * from "./dropdownData";
 export * from "./gridData";
