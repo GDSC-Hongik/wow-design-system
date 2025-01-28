3b0ac9703c991e4483229532a261b50192786a72
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2025-01-28 15:20:36 +0900

    [Feature] Foundation Icon 페이지 마크업 작성  (#185)
    * feat: Icon 파운데이션 페이지 작성
    
    * fix: img에서 svg로 이미지 변경

---
+++ apps/wow-docs/app/foundation/icon/page.tsx
@@ -0,0 +1,32 @@
+import ImageCards from "@components/ImageCards";
+import Space from "@components/Space";
+import Text from "@components/Text";
+import Title from "@components/Text/Title";
+import { iconDescriptions } from "@constants/document/foundation/icon";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Icon",
+  description: "와우 디자인 시스템의 Icon입니다.",
+};
+
+const IconPage = () => {
+  return (
+    <>
+      <Title
+        main="Icon"
+        sub="아이콘은 간결한 표현을 통해 쉽고 빠르게 정보를 전달하는 것을 목표로 합니다."
+        variant="header"
+      />
+      <Space height={68} />
+      <Text as="h2" typo="display2WebPage">
+        Guideline
+      </Text>
+      <Space height={48} />
+      <ImageCards items={iconDescriptions} />
+      <Space height={200} />
+    </>
+  );
+};
+
+export default IconPage;
