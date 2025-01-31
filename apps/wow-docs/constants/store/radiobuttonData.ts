8ee19a643ced3fe8dacf15d10ed248a58651b6d6
hamo-o <yheel369@gmail.com>
2025-01-31 20:26:02 +0900

    fix: png to svg 데이터 형식 변경

--- apps/wow-docs/constants/store/radiobuttonData.ts
+++ apps/wow-docs/constants/store/radiobuttonData.ts
@@ -5,7 +5,7 @@ export const radioButtonItems: Item[] = [
     main: "Spacing",
     sub: "여러 항목을 사용할 경우 세로로 배치 및 간격 지키기",
     imageAlt: "여러 항목 사용 시 세로 배치 및 8px 간격 유지 안내 이미지",
-    imageSrc: "/radiobutton/spacing.png",
+    imageSrc: "/component/radiobutton/spacing.svg",
     imageWidth: 908,
     imageHeight: 140,
   },
