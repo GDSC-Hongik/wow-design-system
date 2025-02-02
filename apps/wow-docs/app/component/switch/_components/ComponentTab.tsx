405651342a27cdf3739448b4960b17be402123d3
soulchicken <soulfever01@naver.com>
2025-02-02 17:22:35 +0900

    fix: 사용하지 않는 이미지 태그 제거

--- apps/wow-docs/app/component/switch/_components/ComponentTab.tsx
+++ apps/wow-docs/app/component/switch/_components/ComponentTab.tsx
@@ -2,7 +2,6 @@ import Card from "@components/Card";
 import Space from "@components/Space";
 import Text from "@components/Text";
 import { Grid } from "@styled-system/jsx";
-import Image from "next/image";
 import Switch from "wowds-ui/Switch";
 
 import SwitchBtns from "@/component/switch/_components/SwitchBtns";
@@ -16,12 +15,6 @@ const ComponentTab = () => {
       <Text typo="headingWebPage">Btn</Text>
       <Space height={20} />
       <Card style={{ padding: "32px auto" }}>
-        <Image
-          alt="Switch 버튼의 checked, unchecked 상태를 dafault, hover, pressed, disabled로 분류해서 보여주는 이미지"
-          height={188}
-          src="/component/switch/switch-component-btn.svg"
-          width={166}
-        />
         <Grid columnGap="20px" columns={2}>
           <SwitchBtns />
         </Grid>
