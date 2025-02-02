becb70971e8eb97fa1b215fc57c9235ab5f6e9b2
Eugene Kim <eugene02876@gmail.com>
2025-02-02 14:51:02 +0900

    refac: Box 컴포넌트에 disabled 속성 추가

---
+++ apps/wow-docs/app/component/box/_component/GuidelineTab.tsx
@@ -0,0 +1,50 @@
+import ImageCards from "@components/ImageCards";
+import Space from "@components/Space";
+import Text from "@components/Text";
+import {
+  combinationButtonItems,
+  widthButtonItems,
+  withIconButtonItems,
+  withSubjectButtonItems,
+} from "@constants/store/buttonData";
+import Divider from "wowds-ui/Divider";
+
+const GuidelineTab = () => {
+  return (
+    <>
+      <Space height={48} />
+      <Text as="h2" typo="display2WebPage">
+        Guideline
+      </Text>
+      <Space height={40} />
+      <Text as="h3" typo="headingWebPage">
+        Width
+      </Text>
+      <Space height={40} />
+      <ImageCards items={widthButtonItems} />
+      <Divider />
+      <Space height={40} />
+      <Text as="h3" typo="headingWebPage">
+        Combination
+      </Text>
+      <Space height={40} />
+      <ImageCards items={combinationButtonItems} />
+      <Divider />
+      <Space height={40} />
+      <Text as="h3" typo="headingWebPage">
+        With Icon
+      </Text>
+      <Space height={40} />
+      <ImageCards items={withIconButtonItems} />
+      <Divider />
+      <Space height={40} />
+      <Text as="h3" typo="headingWebPage">
+        With Subject
+      </Text>
+      <Space height={40} />
+      <ImageCards items={withSubjectButtonItems} />
+    </>
+  );
+};
+
+export default GuidelineTab;
